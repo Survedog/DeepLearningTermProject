ffdf8dbb780500fd6c13cb53144d5d9482cb0ae7
survedog <survedog@gmail.com>
2023-11-28 22:28:24 +0900

    Konlpy 한국어 파서 라이브러리 추가

--- Pycharm/DeepLearning2023/main.py
+++ Pycharm/DeepLearning2023/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+from konlpy.tag import Kkma
+
+parser = Kkma()
+
+if __name__ == '__main__':
+    words = parser.morphs('안녕하세요. 저는 프로그래머입니다. 만나서 반갑습니다.')
+    print(words)
