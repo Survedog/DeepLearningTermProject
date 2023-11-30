fce5bfe2fe0ade70d41120ea6762fc019566bfd2
survedog <survedog@gmail.com>
2023-11-30 10:00:22 +0900

    임베딩 닷 레이어 구현

--- Pycharm/DeepLearning2023/embedding_layer.py
+++ Pycharm/DeepLearning2023/embedding_layer.py
@@ -22,9 +22,34 @@ class EmbeddingLayer(LayerBase):
         return weight[word_ids]
 
     def backward(self, dout):
-        dW = self.grads[0]
-        dW[...] = np.zeros_like(0)
+        dWeight = self.grads[0]
+        dWeight[...] = np.zeros_like(0)
 
         for dout_idx, word_id, in enumerate(self.word_ids):
-            dW[word_id] += dout[dout_idx]
-        return dW
+            dWeight[word_id] += dout[dout_idx]
+        return dWeight
+
+
+class EmbeddingDotLayer(LayerBase):
+
+    def __init__(self, weight):
+        super().__init__()
+        self.params.append(weight)
+        self.grads.append(np.zeros_like(weight))
+        self.embed_layer = EmbeddingLayer(weight)
+        self.cache = None
+
+    def forward(self, word_ids, h):
+        embedded = self.embed_layer.forward(word_ids)
+        dotted = h * embedded
+        self.cache = (h, embedded)
+        return np.sum(dotted, axis=1)
+
+    def backward(self, dout):
+        h, embedded = self.cache
+        dout = dout.reshape(dout.shape[0], 1)
+
+        dEmbed = h * dout
+        self.embed_layer.backward(dEmbed)
+        dh = embedded * dout
+        return dh
--- Pycharm/DeepLearning2023/tests/test_embedding_layer.py
+++ Pycharm/DeepLearning2023/tests/test_embedding_layer.py
@@ -2,23 +2,55 @@ import unittest
 from embedding_layer import *
 from utils import get_one_hot_encoding
 
+
 class EmbeddingLayerTests(unittest.TestCase):
 
     def test_forward_and_backward(self):
-        weight = np.random.rand(4, 5)
+        word_ids = [1, 2, 1]
+        weight = np.array([[0, 1, 2, 3, 4],
+                           [5, 6, 7, 8, 9],
+                           [4, 3, 2, 1, 0],
+                           [9, 8, 7, 6, 5]])
         embed_layer = EmbeddingLayer(weight)
 
-        word_ids = [1, 2, 1]
         embedded = embed_layer.forward(word_ids)
         self.assertTrue(np.array_equal(embedded, weight[word_ids]))
 
-        dout = np.random.rand(embedded.shape[0], embedded.shape[1])
-        dW = embed_layer.backward(dout)
+        dout = np.array([[0, 1, 2, 3, 4],
+                         [5, 6, 7, 8, 9],
+                         [9, 8, 7, 6, 5]])
+        dWeight = embed_layer.backward(dout)
 
-        answer = np.zeros_like(weight)
-        for idx, word_id in enumerate(word_ids):
-            answer[word_id] += dout[idx]
-        self.assertTrue(np.array_equal(dW, answer))
+        self.assertTrue(np.array_equal(dWeight, np.array([[0, 0, 0, 0, 0],
+                                                          [9, 9, 9, 9, 9],
+                                                          [5, 6, 7, 8, 9],
+                                                          [0, 0, 0, 0, 0]])))
 
 
+class EmbeddingDotLayerTests(unittest.TestCase):
 
+    def test_forward_and_backward(self):
+        word_ids = [1, 2, 1]
+        weight = np.array([[0, 1, 2, 3, 4],
+                           [5, 6, 7, 8, 9],
+                           [4, 3, 2, 1, 0],
+                           [9, 8, 7, 6, 5]])
+        h = np.array([[0, 1, 2, 3, 4],
+                      [5, 6, 7, 8, 9],
+                      [9, 8, 7, 6, 5]])
+
+        embed_dot_layer = EmbeddingDotLayer(weight)
+        point = embed_dot_layer.forward(word_ids, h)
+
+        # dot result
+        # [0, 6, 14, 24, 36],
+        # [20, 18, 14, 8, 0],
+        # [45, 48, 49, 48, 45]
+        self.assertTrue(np.array_equal(point, np.array([80, 60, 235])))
+
+        dout = np.array([0, 1, 2])
+        dh = embed_dot_layer.backward(dout)
+
+        self.assertTrue(np.array_equal(dh, np.array([[0, 0, 0, 0, 0],
+                                                     [4, 3, 2, 1, 0],
+                                                     [10, 12, 14, 16, 18]])))
