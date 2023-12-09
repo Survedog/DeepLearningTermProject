8e806e44a16bb0ffb03e9af48759daf1d735092c
survedog <survedog@gmail.com>
2023-12-10 00:04:58 +0900

    에세이 평가 모델에 멤버 변수로 임베딩 계층, RNN모델 추가

--- Pycharm/DeepLearning2023/eval_model/essay_eval_model.py
+++ Pycharm/DeepLearning2023/eval_model/essay_eval_model.py
@@ -1,12 +1,16 @@
 from common.base_layer import LayerBase
+from common.affine_layer import AffineLayer
+from word2vec.embedding_layer import EmbeddingLayer
 
 
 class EssayEvalModel(LayerBase):
 
-    def __init__(self):
+    def __init__(self, embed_weight, rnn_model):
         super().__init__()
+        self.embed_in_layer = EmbeddingLayer(embed_weight)
+        self.rnn_model = rnn_model
 
-    def forward(self):
+    def forward(self, x):
         pass
 
     def backward(self):
