d3ad9beeeabfc88833297074585fb4af470c9ecb
survedog <survedog@gmail.com>
2023-12-11 16:00:45 +0900

    언어 모델 학습 후 평가에 필요한 테스트 데이터를 py.array로 변환 후 입력

--- Pycharm/DeepLearning2023/lang_model/train_language_model.py
+++ Pycharm/DeepLearning2023/lang_model/train_language_model.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     train_data_list = get_processed_essay_data(load_test_data=False,
                                                word_to_id=word_to_id,
                                                load_pickle=True)
-    train_data_list = train_data_list[:10]
+    train_data_list = train_data_list[:1000]
 
     vocab_size = len(id_to_word)
     wordvec_size = 100
@@ -41,11 +41,11 @@ if __name__ == '__main__':
                                               word_to_id=word_to_id,
                                               load_pickle=True)
     random.shuffle(test_data_list)
-    test_data_list = test_data_list[:1]
+    test_data_list = test_data_list[:1000]
 
     total_loss = 0
     for test_data in test_data_list:
-        essay_corpus = sum(test_data['paragraph'], [])
+        essay_corpus = py.array(sum(test_data['paragraph'], []))
         xs = essay_corpus[:-1]
         ts = essay_corpus[1:]
 
@@ -53,5 +53,5 @@ if __name__ == '__main__':
         total_loss += model.forward(xs, ts)
 
     perplexity = py.exp(total_loss / len(test_data_list))
-    print('테스트 perplexity: %0.2f' % perplexity)
+    print('Test perplexity: %0.2f' % perplexity)
 
