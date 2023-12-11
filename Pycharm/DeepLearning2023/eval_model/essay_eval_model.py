8b4fdb943f9b501b7016ebd9d522681a8b04d723
survedog <survedog@gmail.com>
2023-12-12 00:38:31 +0900

    에세이 평가 모델에 SSE loss 레이어 추가 모델 내 아핀 계층 가중치 크기 결정

--- Pycharm/DeepLearning2023/eval_model/essay_eval_model.py
+++ Pycharm/DeepLearning2023/eval_model/essay_eval_model.py
@@ -1,62 +1,34 @@
 from common.base_layer import LayerBase
 from common.affine_layer import AffineLayer
 from common.utils import py, load_data, save_data
-from word2vec.embedding_layer import EmbeddingLayer
+from common.loss_layers import SSELossLayer
 import math
 
 
 class EssayEvalModel(LayerBase):
 
-    @classmethod
-    def get_args_from_processed_data(cls, data, time_size=50, load_pickle=True, save_pickle=False, pickle_name='eval_model_args.p'):
-        if load_pickle:
-            args = load_data(pickle_name)
-            if args is not None:
-                return args
-
-        xs = []
-        for paragraph in data['paragraph']:
-            split_amount = math.ceil(len(paragraph) / time_size)
-
-            paragraph = py.array(paragraph)
-            paragraph.resize((1, split_amount * time_size))  # time size에 맞는 크기가 될 때까지 내용을 반복시킨다.
-            paragraph = paragraph.reshape(-1, time_size)
-            xs.append(paragraph)
-        xs = py.array(xs)
-
-        org_weight, cont_weight, exp_weight = data['weight']['org'], data['weight']['cont'], data['weight']['exp']
-        score_metrics = [py.array([org_weight['org'], org_weight['org_paragraph'], org_weight['org_essay'], org_weight['org_coherence'], org_weight['org_quantity']]),
-                         py.array([cont_weight['con'], cont_weight['con_clearance'], cont_weight['con_novelty'], cont_weight['con_prompt'], cont_weight['con_description']]),
-                         py.array([exp_weight['exp'], exp_weight['exp_grammar'], exp_weight['exp_vocab'], exp_weight['exp_style']], data['corr_count'])]
-
-        t = []
-        for score_list in data['score']:
-            t += sum(score_list, [])
-
-        if save_pickle:
-            args = xs, score_metrics, t
-            save_data(pickle_name, args)
-
-        return xs, score_metrics, t
-
-    def __init__(self, rnn_model, fit_premade_models=False):
+    def __init__(self, rnn_model, time_size=50, fit_premade_models=False):
         super().__init__()
         self.cache = None
 
-        exp_affine_weight = py.random.randn()
-        exp_affine_bias = py.random.randn()
-        org_affine_weight = py.random.randn()
-        org_affine_bias = py.random.randn()
-        cont_affine_weight = py.random.randn()
-        cont_affine_bias = py.random.randn()
+        affine_input_size = time_size + 4
+        exp_metrics_amount, org_metrics_amount, cont_metrics_amount = 3, 4, 4
+
+        randn = py.random.randn
+        exp_affine_weight = randn(affine_input_size, exp_metrics_amount * 3, dtype='f')
+        exp_affine_bias = randn(exp_metrics_amount * 3, dtype='f')
+        org_affine_weight = randn(affine_input_size, org_metrics_amount * 3, dtype='f')
+        org_affine_bias = randn(org_metrics_amount * 3, dtype='f')
+        cont_affine_weight = randn(affine_input_size, cont_metrics_amount * 3, dtype='f')
+        cont_affine_bias = randn(cont_metrics_amount * 3, dtype='f')
 
         self.rnn_model = rnn_model
+        self.exp_affine_layer = AffineLayer(exp_affine_weight, exp_affine_bias)
         self.org_affine_layer = AffineLayer(org_affine_weight, org_affine_bias)
         self.cont_affine_layer = AffineLayer(cont_affine_weight, cont_affine_bias)
-        self.exp_affine_layer = AffineLayer(exp_affine_weight, exp_affine_bias)
-        self.loss_layer = None
 
         self.layers = [self.rnn_model, self.org_affine_layer, self.cont_affine_layer, self.exp_affine_layer]
+        self.loss_layer = SSELossLayer()
 
         train_from = 0
         if not fit_premade_models:
@@ -103,4 +75,37 @@ class EssayEvalModel(LayerBase):
 
     def reset_state(self):
         self.rnn_model.reset_state()
+
+    @classmethod
+    def get_args_from_processed_data(cls, data, time_size=50, load_pickle=True, save_pickle=False, pickle_name='eval_model_args.p'):
+        if load_pickle:
+            args = load_data(pickle_name)
+            if args is not None:
+                return args
+
+        xs = []
+        for paragraph in data['paragraph']:
+            split_amount = math.ceil(len(paragraph) / time_size)
+
+            paragraph = py.array(paragraph)
+            paragraph.resize((1, split_amount * time_size))  # time size에 맞는 크기가 될 때까지 내용을 반복시킨다.
+            paragraph = paragraph.reshape(-1, time_size)
+            xs.append(paragraph)
+        xs = py.array(xs)
+
+        org_weight, cont_weight, exp_weight = data['weight']['org'], data['weight']['cont'], data['weight']['exp']
+        # 대분류 가중치를 그에 해당하는 소분류 가중치에 곱한다.
+        score_metrics = [exp_weight['exp'] * py.array([exp_weight['exp_grammar'], exp_weight['exp_vocab'], exp_weight['exp_style'], data['corr_count']]),  # 문법 점수 계산에 교정 횟수를 포함
+                         org_weight['org'] * py.array([org_weight['org_paragraph'], org_weight['org_essay'], org_weight['org_coherence'], org_weight['org_quantity']]),
+                         cont_weight['con'] * py.array([cont_weight['con_clearance'], cont_weight['con_novelty'], cont_weight['con_prompt'], cont_weight['con_description']])]
+
+        t = []
+        for score_list in data['score']:
+            t += sum(score_list, [])
+
+        if save_pickle:
+            args = xs, score_metrics, t
+            save_data(pickle_name, args)
+
+        return xs, score_metrics, t
 
