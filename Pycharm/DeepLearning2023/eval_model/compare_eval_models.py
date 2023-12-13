b29115ce9cdcc55a98ca1a70dffbc3c3be8fdbd7
survedog <survedog@gmail.com>
2023-12-13 15:43:41 +0900

    에세이 모델 비교 코드 구조 변경

--- Pycharm/DeepLearning2023/eval_model/compare_eval_models.py
+++ Pycharm/DeepLearning2023/eval_model/compare_eval_models.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     test_data_list = get_processed_essay_data(load_test_data=True, word_to_id=word_to_id, load_pickle=True)
     random.shuffle(test_data_list)
 
+    # 모델 설정
     embed_weight, _ = load_data('cbow_params.p')
     vocab_size = len(id_to_word)
     wordvec_size = embed_weight.shape[-1]
@@ -27,34 +28,47 @@ if __name__ == '__main__':
     lstm2_hidden_size = 11
     time_size = 80
 
-    train_data_start = 0
-    train_data_size = 1000
-    test_data_size = 1000
-    train_max_count = 4
-    save_param = True
-
-    test_plot_x = []
-    test_plot_y1 = []
-    test_plot_y2 = []
-
     print('Creating model...')
-    eval_model1 = EssayEvalModel(vocab_size, wordvec_size, lstm1_hidden_size, time_size, embed_weight=embed_weight, dropout_rate=0.5)
-    eval_model2 = EssayEvalModel2(vocab_size, wordvec_size, lstm1_hidden_size, lstm2_hidden_size, time_size, embed_weight=embed_weight, dropout_rate=0.3)
+    eval_model1 = EssayEvalModel(vocab_size, wordvec_size, lstm1_hidden_size, time_size, embed_weight=embed_weight,
+                                 dropout_rate=0.5)
+    eval_model2 = EssayEvalModel2(vocab_size, wordvec_size, lstm1_hidden_size, lstm2_hidden_size, time_size,
+                                  embed_weight=embed_weight, dropout_rate=0.3)
     optimizer1 = AdamOptimizer()
     optimizer2 = AdamOptimizer()
     trainer1 = EssayEvalModelTrainer(eval_model1, optimizer1)
     trainer2 = EssayEvalModelTrainer(eval_model2, optimizer2)
 
-    for train_count in range(train_max_count):
-        train_data_start += train_data_size
+    # 학습 설정
+    max_epoch = 3
+    train_data_size = 10000
+    test_data_size = 1000
+    max_compare_count = 4
 
-        print('Loading train data...')
-        x_train, t_train = EssayEvalModel.get_x_t_list_from_processed_data(train_data_list[train_data_start: train_data_start + train_data_size], time_size=time_size, load_pickle=False, save_pickle=False)
-        x_test, t_test = EssayEvalModel.get_x_t_list_from_processed_data(test_data_list[:test_data_size], time_size=time_size, load_pickle=False, save_pickle=False)
+    train_per_compare = train_data_size // max_compare_count
+    test_per_compare = test_data_size // max_compare_count
+    save_param = True
+
+    print('Loading train/test data...')
+    x_train, t_train = EssayEvalModel.get_x_t_list_from_processed_data(train_data_list[: train_data_size],
+                                                                       time_size=time_size, load_pickle=False,
+                                                                       save_pickle=False)
+    x_test, t_test = EssayEvalModel.get_x_t_list_from_processed_data(test_data_list[:test_data_size],
+                                                                     time_size=time_size, load_pickle=False,
+                                                                     save_pickle=False)
+
+    # plot
+    test_plot_x = []
+    test_plot_y1 = []
+    test_plot_y2 = []
+
+    train_from, test_from = 0, 0
+    for train_count in range(max_compare_count):
+        train_end = train_from + train_per_compare
+        test_end = test_from + test_per_compare
 
         print('Start fitting...')
-        trainer1.fit(x_train, t_train, max_epoch=1)
-        trainer2.fit(x_train, t_train, max_epoch=1)
+        trainer1.fit(x_train[train_from:train_end], t_train[train_from:train_end], max_epoch=max_epoch)
+        trainer2.fit(x_train[train_from:train_end], t_train[train_from:train_end], max_epoch=max_epoch)
 
         if save_param:
             eval_model1.save_params()
@@ -63,7 +77,7 @@ if __name__ == '__main__':
         print('Start testing...')
         total_loss1, total_loss2 = 0.0, 0.0
         loss_count = 0
-        for x, t in zip(x_test, t_test):
+        for x, t in zip(x_test[test_from:test_end], t_test[test_from:test_end]):
             loss_count += 1
             eval_model1.reset_state()
             eval_model2.reset_state()
@@ -71,9 +85,12 @@ if __name__ == '__main__':
             total_loss2 += eval_model2.forward(x, t, train_flag=False)
         print('Test - Loss1: %.2f, Loss2: %.2f' % (total_loss1/loss_count, total_loss2/loss_count))
 
-        test_plot_x.append(train_data_start)
+        test_plot_x.append(train_from)
         test_plot_y1.append(float(total_loss1 / loss_count))
         test_plot_y2.append(float(total_loss2 / loss_count))
+
+        train_from += train_per_compare
+        test_from += test_per_compare
 
     plt.plot(test_plot_x, test_plot_y1, 'r-', label='EEM1')
     plt.plot(test_plot_x, test_plot_y2, 'b-', label='EEM2')
